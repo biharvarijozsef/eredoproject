28e7a93e76e388d457e4982a6acaf46fdd0a36c1
jbiharva <biharvarijozsef@hotmail.com>
2015-06-26 15:57:45 +0000

    lkj

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require("mongoose");
+var jobModel = require("./models/Job");
 var app = express();
 
 app.set('view engine', 'jade');
@@ -6,9 +8,26 @@ app.set('views', __dirname);
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
+
+
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection) {
+        res.send(collection);
+    })
+});
+app.get('*', function(req, res){
   res.render('index');
 });
 
+//mongoose.connect('mongodb://localhost/eredoproject');
+mongoose.connect('mongodb://jbiharva:1qay2wsx@ds043062.mongolab.com:43062/eredoproject');
+
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+   console.log('connected to mongodb successfully!'); 
+   jobModel.seedJobs();
+});
 // Note: Uses the CLOUD9 port
 app.listen(process.env.PORT);
